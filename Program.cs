277c8f84d95ccacf872d486346dca7ee19d31509
ahmad abd <ahmadabd@users.noreply.github.com>
2018-02-14 23:44:55 +0330

    using Monitor in Thread
    it show how to using monitor for syncing all threads

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace getProcessMyPc
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            sync syncObj = new sync();
+            Thread[] threads = new Thread[1000];
+            for(int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(new ParameterizedThreadStart(syncObj.add));
+            }
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Start(22);
+            }
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Join();
+            }
+            Console.WriteLine(syncObj.count());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ sync.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace getProcessMyPc
+{
+    class sync
+    {
+        List<object> listOfNum = new List<object>();
+        object lck = new object();
+        public void add(object number)
+        {
+            try
+            {
+                Monitor.Enter(lck) ;
+                if (!listOfNum.Contains(number))
+                {
+                    listOfNum.Add(number);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("exception");
+            }
+            finally
+            {
+                Monitor.Exit(lck);
+            }
+        }
+        public int count()
+        {
+            return listOfNum.Count;
+        }
+    }
+}
